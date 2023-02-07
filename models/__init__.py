7a57cf27fec0db09abe37791697a885b0b6b2d49
Ibrah44 <107178042+Ibrah44@users.noreply.github.com>
2023-02-07 08:49:38 +0300

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""__init__ method for models package, or
-Module for FileStorage autoinit."""
-
+"""Initializes the package"""
 from models.engine.file_storage import FileStorage
-
 storage = FileStorage()
 storage.reload()
